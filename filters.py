effcae274f86b1201fad2c354b766f8fb4d749a7
김융 <sysl6646@naver.com>
2023-10-23 05:39:17 +0900

    20231023 still crashing on some processing

--- filters.py
+++ filters.py
@@ -3,10 +3,7 @@ import numpy as np
 
 def apply_filter(imgpath, sobelw, cannyw, lapw):
     img = cv2.imread(imgpath, 0).astype(np.uint8)
-    ker_sharpen = np.array([
-        [1, -2, 1],
-        [-2, 4, -2],
-        [1, -2, 1]])
+
 
     dst1_sobel = cv2.Sobel(img, ddepth=-1,
                            dx=1, dy=1,
@@ -23,5 +20,10 @@ def apply_gate(target, thresh):
 def apply_denoise(target, strength, thresh):
     print("---")
     res = cv2.fastNlMeansDenoising(target, None, strength, strength, 5, 21)
+    ker_sharpen = np.array([
+        [1, -2, 1],
+        [-2, 4, -2],
+        [1, -2, 1]])
+    #res = cv2.filter2D(target, -1, ker_sharpen)
     res[res<thresh] = 0
     return res
